dfd6c91e4ddecbe4e2adf84b9629125a0418a517
Gerd Reiss <gerd@reiss.pro>
2021-03-02 21:09:03 +0100

    Learning ZIO: ZIONOMICON Chapter 4 remove Main.scala

--- zionomicon/exercises/chapter04/src/main/scala/Main.scala
+++
@@ -1,7 +0,0 @@
-import zio._
-import console._
-
-object Main extends App {
-  override def run(args: List[String]): URIO[ZEnv, ExitCode] =
-    ZIO.effect(putStrLn("Hello, Zionomicon Chapter 4")).exitCode
-}
