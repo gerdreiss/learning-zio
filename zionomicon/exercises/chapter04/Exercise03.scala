f48483279c38d9945d16f7e596dd848c4e055232
Gerd Reiss <gerd@reiss.pro>
2021-03-03 10:34:33 +0100

    ZIONOMICON Chapter 4 Exercise 3

---
+++ zionomicon/exercises/chapter04/Exercise03.scala
@@ -0,0 +1,13 @@
+import zio._
+
+// Using the ZIO#foldCauseM operator and the Cause#prettyPrint method,
+// implement an operator that takes an effect, and returns a new effect
+// that logs any failures of the original effect (including errors and defects),
+// without changing its failure or success value.
+object Exercise03 {
+  def logFailures[R, E, A](zio: ZIO[R, E, A]): ZIO[R, E, A] =
+    zio.foldCauseM(
+      _ => zio.tapCause(cause => ZIO.effectTotal(println(cause.prettyPrint))),
+      _ => zio
+    )
+}
