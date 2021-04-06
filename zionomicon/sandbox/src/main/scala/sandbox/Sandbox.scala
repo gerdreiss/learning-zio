af617f99b58dc38a43bcee302a430ced4ecc70aa
Gerd Reiss <gerd@reiss.pro>
2021-04-06 10:29:45 +0200

    ZIONOMICON Chapter 15 - ZManaged, parallel

--- zionomicon/sandbox/src/main/scala/sandbox/Sandbox.scala
+++ zionomicon/sandbox/src/main/scala/sandbox/Sandbox.scala
@@ -1,37 +1,41 @@
 package sandbox
 
-import java.io.File
-import java.nio.file.Files
-import java.nio.file.Paths
-import java.util.stream.Collectors
-
-import scala.io.Source
-
+import zio.ZIO._
 import zio._
+import zio.console._
 
-import ZIO._
-import console._
-import collection.JavaConverters._
+import java.io.File
+import scala.io.Source
 
 object Sandbox extends App {
 
-  lazy val names = new File(".").list.toList
-  lazy val file  = (name: String) => ZManaged.succeed(new File(name))
-  lazy val files = ZManaged.foreach(names)(file)
+  lazy val names: List[String] =
+    new File(".").list.toList
+
+  lazy val file: String => UManaged[File] =
+    (name: String) => ZManaged.succeed(new File(name))
+
+  lazy val files: UManaged[List[File]] =
+    ZManaged.foreach(names)(file)
 
   lazy val currentDirFileContent: Task[List[String]] =
     files.use { files =>
       Task
         .foreach(files) { file =>
-          ifM(ZIO.succeed(file.isFile()))(
-            Task(Source.fromFile(file).getLines.toList),
+          ifM(ZIO.succeed(file.isFile))(
+            ZIO.bracket(
+              Task(Source.fromFile(file)),
+              (source: Source) => ZIO.succeed(source.close()),
+              (source: Source) => Task(source.getLines.toList)
+            ),
             ZIO.succeed(List.empty)
           )
         }
         .map(_.flatten)
     }
 
-  lazy val parFiles = file(".scalafmt.conf").zipPar(file("build.sbt"))
+  lazy val parFiles: UManaged[(File, File)] =
+    file(".scalafmt.conf").zipPar(file("build.sbt"))
 
   override def run(args: List[String]): URIO[ZEnv, ExitCode] =
     // currentDirFileContent
@@ -39,12 +43,19 @@ object Sandbox extends App {
     //   .exitCode
     parFiles
       .use { case (scalafmt, build) =>
-        ZIO(
-          (
-            Source.fromFile(scalafmt).getLines.mkString("\n"),
-            Source.fromFile(build).getLines.mkString("\n")
+        ZIO
+          .bracket(
+            Task(Source.fromFile(scalafmt)),
+            (scalafmtS: Source) => ZIO.succeed(scalafmtS.close()),
+            (scalafmtS: Source) => Task(scalafmtS.getLines.mkString("\n"))
           )
-        )
+          .zipPar(
+            ZIO.bracket(
+              Task(Source.fromFile(build)),
+              (buildS: Source) => ZIO.succeed(buildS.close()),
+              (buildS: Source) => Task(buildS.getLines.mkString("\n"))
+            )
+          )
       }
       .flatMap { case (scalafmt, build) =>
         putStrLn(scalafmt + "\n" + build)
